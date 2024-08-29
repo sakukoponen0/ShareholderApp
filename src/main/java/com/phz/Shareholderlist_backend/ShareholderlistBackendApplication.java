694c466e6875f534bd90613af857250de4e6eb5b
sakukoponenLAB <saku.koponen@student.lab.fi>
2024-08-29 11:19:06 +0300

    Initialized repo

---
+++ src/main/java/com/phz/Shareholderlist_backend/ShareholderlistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.phz.Shareholderlist_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareholderlistBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareholderlistBackendApplication.class, args);
+	}
+
+}
